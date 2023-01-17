fa9fdf74c4b16c7f019b83f6ea422bd82b1c7cd3
Guillaume G <guillaume.gerard2112@outlook.fr>
2023-01-17 14:48:22 +0100

    fix css

--- app_game_of_thrones/src/components/CharactersList/index.tsx
+++ app_game_of_thrones/src/components/CharactersList/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Character } from '../../models/Character';
 
 const characters:Character[] = [
-    Character (1, 'Arya Starck', 'https://static.posters.cz/image/1300/art-photo/game-of-thrones-arya-stark-i135445.jpg'),
-    Character (2, 'Robb Starck', 'https://figurinepop.com/public/robbstark1_1.jpg'),
+    Character (1, 'Arya Starck', 'https://static1.terrafemina.com/articles/7/33/37/47/@/482477-game-of-thrones-maisie-williams-950x635-1.jpg'),
+    Character (2, 'Robb Starck', 'https://images7.alphacoders.com/581/581625.jpg'),
     Character (3, 'Tyrion Lannister', 'https://images.rtl.fr/rtl/www/1348813-selon-une-theorie-pour-la-saison-tyrion-lannister-serait-un-targaryen.jpg'),
     Character (4, 'Daenerys Targaryen', 'https://www.leparisien.fr/resizer/t0I3f974EkGmDiq1J1gfzLmKWrI=/932x582/cloudfront-eu-central-1.images.arcpublishing.com/leparisien/R7URXFFJLSJZCQWZ3W5PZXBRIQ.jpg')
 ]
@@ -18,7 +18,7 @@ const CharactersList : React.FC = () =>
                 <img 
                     src={c.imageURL}
                     alt="Avatar" 
-                    style={{ width: '100%', maxWidth: '500px'}}
+                    style={{ width: '100%'}}
                     >
                 </img>
                 <div className="container">
