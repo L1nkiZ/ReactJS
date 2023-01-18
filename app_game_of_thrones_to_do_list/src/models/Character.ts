aeb85ba7ff7018357b4372889a96d096ad7a1190
Guillaume G <guillaume.gerard2112@outlook.fr>
2023-01-18 09:27:40 +0100

    update game of thrones caract

--- app_game_of_thrones_to_do_list/src/models/Character.ts
+++ app_game_of_thrones_to_do_list/src/models/Character.ts
@@ -1,16 +1,22 @@
 export interface Character {
     id:number,
     name:string,
-    imageURL:string
+    imageURL:string,
+    titre: string,
+    famille: string,
 }
 
 export const Character =(
     id : number,
     name: string,
-    imageURL : string
+    imageURL : string,
+    titre : string,
+    famille : string,
 
 ): Character =>({
     id,
     name,
-    imageURL
+    imageURL,
+    titre,
+    famille,
 });
